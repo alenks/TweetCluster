cc348fbe1b05adcc797d2d4f0733390456548ecd
Alen K S <alen@Alens-MBP.lan>
2020-11-07 23:40:22 +0800

    Minor changes

--- src/Main.scala
+++ src/Main.scala
@@ -1,17 +1,26 @@
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.ml.feature.Word2VecModel
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
 
 object Main {
+// Using ml.clustering.KMeans (the DataFrame based API)
   def main(args: Array[String]) {
     val spark = SparkSession.builder
       .appName("Simple Application")
       .master("local[*]")
       .getOrCreate()
+    val sc = SparkContext.getOrCreate()
     import spark.implicits._
 
-    // write your code here
+    val input = sc.textFile(args(0)).map(line => line.split(" ").toSeq)
+    val word2vec = new Word2Vec()
+    val model = word2vec.fit(input)
+    val synonyms = model.findSynonyms("1", 5)
+    //val stopwords = spark.read.textFile(args(1))
+
 
     spark.stop()
   }
